7da8a3ffb8d3bc3119a2ca1a96a4f10e062a2665
CodemanFalcon <Cody.Salyi@hyland.com>
2022-12-13 01:40:10 -0500

    feat: day 4 complete

---
+++ day-4/camp-cleanup.py
@@ -0,0 +1,18 @@
+input_file = open('input.txt', 'r')
+
+lines = input_file.readlines()
+counter = 0
+for line in lines:
+    assignments = line.split(",")
+    assignment1 = assignments[0].split("-")
+    assignment1range = set(range(int(assignment1[0]), int(assignment1[1])+1))
+
+    assignment2 = assignments[1].split("-")
+    assignment2range = set(range(int(assignment2[0]), int(assignment2[1])+1))
+
+    if assignment1range.issubset(assignment2range):
+        counter+=1
+    elif assignment2range.issubset(assignment1range):
+        counter+=1
+
+print(counter)
